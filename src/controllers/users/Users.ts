6366c35114f8c0fb1f0be5af3c1b6badd009b8db
Swan <me@swan.moe>
2019-12-10 09:51:03 -0500

    Implement user profile tab controllers

--- src/controllers/users/Users.ts
+++ src/controllers/users/Users.ts
@@ -23,14 +23,150 @@ export default class Users {
                 return res.status(404).json({ status: 404, error: "User not found" });
 
             switch (tab) {
+                case "best":
+                    return await Users.GetBestScores(req, res, user, mode);
+                case "recent":
+                    return await Users.GetRecentScores(req, res, user, mode);
+                case "firstplace":
+                    return await Users.GetFirstPlaceScores(req, res, user, mode);
+                case "mapsets":
+                    return await Users.GetUploadedMapsets(req, res, user, mode);
+                case "playlists":
+                    return await Users.GetPlaylists(req, res, user, mode);
                 default:
-                    Responses.Send(req, res, "user-info", `${user.info.username}'s Profile | Quaver`, { 
+                    Responses.Send(req, res, "user-profile-info", `${user.info.username}'s Profile | Quaver`, { 
                         user, 
                         mode
                     });
                     break;
             }
         } catch (err) {
+            Logger.Error(err);
+            Responses.Return500(req, res);
+        }
+    }
+
+    /**
+     * Renders the page that displays the user's best scores
+     * @param req 
+     * @param res 
+     * @param user 
+     * @param mode 
+     */
+    private static async GetBestScores(req: any, res: any, user: any, mode: number): Promise<void> {
+        try {
+            const page = parseInt(req.query.page) || 0;
+
+            let scores = [];
+
+            const apiScores = await API.GET(req, `v1/users/scores/best?id=${user.info.id}&mode=${mode}&page=${page}`);
+            scores = apiScores.scores;
+
+            Responses.Send(req, res, "user-profile-best", `${user.info.username}'s Profile | Quaver`, {
+                user,
+                mode,
+                scores
+            });
+        } catch (err) {
+            Logger.Error(err);
+            Responses.Return500(req, res);
+        }
+    }
+
+    /**
+     * Renders th epage that displays the user's recent scores
+     * @param req 
+     * @param res 
+     * @param user 
+     * @param mode 
+     */
+    private static async GetRecentScores(req: any, res: any, user: any, mode: number): Promise<void> {
+        try {
+            const page = parseInt(req.query.page) || 0;
+
+            let scores = [];
+
+            const apiScores = await API.GET(req, `v1/users/scores/recent?id=${user.info.id}&mode=${mode}&page=${page}`);
+            scores = apiScores.scores;
+
+            Responses.Send(req, res, "user-profile-recent", `${user.info.username}'s Profile | Quaver`, {
+                user,
+                mode,
+                scores
+            });
+        } catch (err) {
+            Logger.Error(err);
+            Responses.Return500(req, res);
+        }
+    }
+
+        /**
+     * Renders th epage that displays the user's first place scores
+     * @param req 
+     * @param res 
+     * @param user 
+     * @param mode 
+     */
+    private static async GetFirstPlaceScores(req: any, res: any, user: any, mode: number): Promise<void> {
+        try {
+            const page = parseInt(req.query.page) || 0;
+
+            let scores = [];
+
+            const apiScores = await API.GET(req, `v1/users/scores/firstplace?id=${user.info.id}&mode=${mode}&page=${page}`);
+            scores = apiScores.scores;
+
+            Responses.Send(req, res, "user-profile-first-place", `${user.info.username}'s Profile | Quaver`, {
+                user,
+                mode,
+                scores
+            });
+        } catch (err) {
+            Logger.Error(err);
+            Responses.Return500(req, res);
+        }
+    }
+
+    /**
+     * Renders the page which displays the user's uploaded mapsets
+     * @param req 
+     * @param res 
+     * @param user 
+     */
+    private static async GetUploadedMapsets(req: any, res: any, user: any, mode: number): Promise<void> {
+        try {
+            const ranked = await API.GET(req, `v1/users/mapsets/${user.info.id}?status=2`);
+            const unranked = await API.GET(req, `v1/users/mapsets/${user.info.id}?status=2`);
+
+            Responses.Send(req, res, "user-profile-mapsets", `${user.info.username}'s Profile | Quaver`, {
+                user,
+                mode,
+                ranked: ranked.mapsets,
+                unranked: unranked.mapsets
+            });
+        } catch (err) {
+            Logger.Error(err);
+            Responses.Return500(req, res);
+        }
+    }
+
+    /**
+     * Renders the page that displays the user's created playlists
+     * @param req 
+     * @param res 
+     * @param user 
+     * @param mode 
+     */
+    private static async GetPlaylists(req: any, res: any, user: any, mode: number): Promise<void> {
+        try {
+            const playlists =  await API.GET(req, `v1/users/${user.info.id}/playlists`);
+
+            Responses.Send(req, res, "user-profile-playlists", `${user.info.username}'s Profile | Quaver`, {
+                user,
+                mode,
+                playlists: playlists.playlists
+            });
+        } catch  (err) {
             Logger.Error(err);
             Responses.Return500(req, res);
         }

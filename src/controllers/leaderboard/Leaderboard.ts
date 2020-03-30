05299a4fbc559cf742adb191e021c8162aaea85b
Daniel <bgdppeu@gmail.com>
2020-03-30 23:37:00 +0300

    Add slug for subpages pagination

--- src/controllers/leaderboard/Leaderboard.ts
+++ src/controllers/leaderboard/Leaderboard.ts
@@ -6,8 +6,8 @@ import LeaderboardType from "./LeaderboardType";
 export default class Leaderboard {
     /**
      * Fetches and returns global/country leaderboards leaderboards
-     * @param req 
-     * @param res 
+     * @param req
+     * @param res
      */
     public static async GET(req: any, res: any): Promise<void> {
         try {
@@ -18,7 +18,7 @@ export default class Leaderboard {
             const stats = await API.GET(req, "v1/stats");
             const limit = 50;
             const totalUsers = stats.stats.total_users;
-            const pages = Math.ceil(totalUsers/limit);
+            const pages = Math.ceil(totalUsers / limit);
 
             const leaderboard = await API.GET(req, `v1/leaderboard?mode=${mode}&page=${page - 1}${country}`);
 
@@ -27,7 +27,8 @@ export default class Leaderboard {
                 leaderboard: leaderboard.users,
                 mode: mode,
                 page: page,
-                pages: pages - 1
+                pages: pages - 1,
+                slug: 'leaderboard'
             });
         } catch (err) {
             Logger.Error(err);
@@ -37,27 +38,28 @@ export default class Leaderboard {
 
     /**
      * Fetches and returns the total hits leaderboard
-     * @param req 
-     * @param res 
+     * @param req
+     * @param res
      */
     public static async TotalHitsGET(req: any, res: any): Promise<void> {
         try {
             const mode = req.query.mode || 1;
             const page = req.query.page || 0;
 
-            const leaderboard = await API.GET(req, `v1/leaderboard/hits?page=${page}`);
+            const leaderboard = await API.GET(req, `v1/leaderboard/hits?page=${page - 1}`);
 
             const stats = await API.GET(req, "v1/stats");
             const limit = 50;
             const totalUsers = stats.stats.total_users;
-            const pages = Math.ceil(totalUsers/limit);
+            const pages = Math.ceil(totalUsers / limit);
 
             Responses.Send(req, res, "leaderboard", "Total Hits Leaderboard | Quaver", {
                 leaderboardType: LeaderboardType.TotalHits,
                 leaderboard: leaderboard.users,
                 mode: mode,
                 page: page,
-                pages: pages
+                pages: pages - 1,
+                slug: 'hits'
             });
         } catch (err) {
             Logger.Error(err);
@@ -67,27 +69,28 @@ export default class Leaderboard {
 
     /**
      * Fetches and returns the multiplayer leaderboard
-     * @param req 
-     * @param res 
+     * @param req
+     * @param res
      */
     public static async MultiplayerGET(req: any, res: any): Promise<void> {
         try {
             const mode = req.query.mode || 1;
-            const page = req.query.page || 0;
+            const page = req.query.page || 1;
 
-            const leaderboard = await API.GET(req, `v1/multiplayer/leaderboard?mode=${mode}&page=${page}`);
+            const leaderboard = await API.GET(req, `v1/multiplayer/leaderboard?mode=${mode}&page=${page - 1}`);
 
             const stats = await API.GET(req, "v1/stats");
             const limit = 50;
             const totalUsers = stats.stats.total_users;
-            const pages = Math.ceil(totalUsers/limit);
+            const pages = Math.ceil(totalUsers / limit);
 
             Responses.Send(req, res, "leaderboard", "Multiplayer Leaderboard | Quaver", {
                 leaderboardType: LeaderboardType.Multiplayer,
                 leaderboard: leaderboard.users,
                 mode: mode,
                 page: page,
-                pages: pages
+                pages: pages - 1,
+                slug: 'multiplayer'
             });
         } catch (err) {
             Logger.Error(err);

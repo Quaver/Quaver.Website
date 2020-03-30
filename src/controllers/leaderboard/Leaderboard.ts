f26a4199db8e770d36818bc0a3bab7e7b2d4b599
Daniel <bgdppeu@gmail.com>
2020-03-30 23:12:02 +0300

    Implement pagination

--- src/controllers/leaderboard/Leaderboard.ts
+++ src/controllers/leaderboard/Leaderboard.ts
@@ -12,7 +12,7 @@ export default class Leaderboard {
     public static async GET(req: any, res: any): Promise<void> {
         try {
             const mode = req.query.mode || 1;
-            const page = req.query.page || 0;
+            const page = req.query.page || 1;
             const country = req.query.country ? `&country=${req.query.country}` : "";
 
             const stats = await API.GET(req, "v1/stats");
@@ -20,14 +20,14 @@ export default class Leaderboard {
             const totalUsers = stats.stats.total_users;
             const pages = Math.ceil(totalUsers/limit);
 
-            const leaderboard = await API.GET(req, `v1/leaderboard?mode=${mode}&page=${page}${country}`);
+            const leaderboard = await API.GET(req, `v1/leaderboard?mode=${mode}&page=${page - 1}${country}`);
 
             Responses.Send(req, res, "leaderboard", "Leaderboard | Quaver", {
                 leaderboardType: country == "" ? LeaderboardType.Global : LeaderboardType.Country,
                 leaderboard: leaderboard.users,
                 mode: mode,
                 page: page,
-                pages: pages
+                pages: pages - 1
             });
         } catch (err) {
             Logger.Error(err);

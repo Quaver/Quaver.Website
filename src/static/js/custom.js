545694a5f971504a2d23a1e59cf28f9cad61543e
Daniel <bgdppeu@gmail.com>
2020-03-31 00:18:05 +0300

    Wait for DOM to load

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -13,64 +13,68 @@ $(window).on('hashchange', function (event) {
     }, 400, 'swing');
 });
 
+document.addEventListener("DOMContentLoaded", function (event) {
+
 // Search
 
-const search = $('#search');
-let searchHasResults = false;
-let searchTimer = null;
-let isHovering = false;
+    const search = $('#search');
+    let searchHasResults = false;
+    let searchTimer = null;
+    let isHovering = false;
 
-search.on('keyup', function () {
-    clearTimeout(searchTimer);
-    let searchText = $(this).val().trim();
+    search.on('keyup', function () {
+        clearTimeout(searchTimer);
+        let searchText = $(this).val().trim();
 
-    if (searchText === "") {
-        $('.searchBox').hide();
-        return;
-    }
+        if (searchText === "") {
+            $('.searchBox').hide();
+            return;
+        }
 
-    searchTimer = setTimeout(function () {
-        $.ajax({
-            type: 'GET',
-            url: apiBaseUrl() + `/v1/users/search/${searchText}`,
-            success: function (response) {
-                if (response.users.length !== 0) {
-                    $('.searchBox').show();
-                    searchHasResults = true;
-                    // Reset results
-                    $('.searchBox ul').html('');
-                    response.users.forEach(function (user) {
-                        $('.searchBox ul').append('<li class="list-group-item align-items-center">' +
-                            `<a href="${baseUrl()}/user/${user.id}">` +
-                            `<img src="${user.avatar_url}" width="50px" height="50px" onerror="this.src='/img/noavatar.jpg'">` +
-                            `<b>${user.username}</b></a></li>`);
-                    });
-                } else {
+        searchTimer = setTimeout(function () {
+            $.ajax({
+                type: 'GET',
+                url: apiBaseUrl() + `/v1/users/search/${searchText}`,
+                success: function (response) {
+                    if (response.users.length !== 0) {
+                        $('.searchBox').show();
+                        searchHasResults = true;
+                        // Reset results
+                        $('.searchBox ul').html('');
+                        response.users.forEach(function (user) {
+                            $('.searchBox ul').append('<li class="list-group-item align-items-center">' +
+                                `<a href="${baseUrl()}/user/${user.id}">` +
+                                `<img src="${user.avatar_url}" width="50px" height="50px" onerror="this.src='/img/noavatar.jpg'">` +
+                                `<b>${user.username}</b></a></li>`);
+                        });
+                    } else {
+                        searchHasResults = false;
+                    }
+                },
+                error: function () {
+                    $('.searchBox').hide();
                     searchHasResults = false;
                 }
-            },
-            error: function () {
-                $('.searchBox').hide();
-                searchHasResults = false;
-            }
-        });
-    }, 300);
-});
+            });
+        }, 300);
+    });
 
-$('.searchBox').hover(function () {
-    isHovering = true;
-}, function () {
-    isHovering = false;
-});
+    $('.searchBox').hover(function () {
+        isHovering = true;
+    }, function () {
+        isHovering = false;
+    });
 
-search.blur(function () {
-    if (!isHovering)
-        $('.searchBox').hide();
-});
+    search.blur(function () {
+        if (!isHovering)
+            $('.searchBox').hide();
+    });
 
-search.focus(function () {
-    if (searchHasResults)
-        $('.searchBox').show();
+    search.focus(function () {
+        if (searchHasResults)
+            $('.searchBox').show();
+    });
+
 });
 
 // Parse duration

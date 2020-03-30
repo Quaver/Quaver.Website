b19d8f0c54dbd0b55ed681e35be6c196d451515d
Daniel <bgdppeu@gmail.com>
2020-03-31 00:41:00 +0300

    Mobile search

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -13,50 +13,22 @@ $(window).on('hashchange', function (event) {
     }, 400, 'swing');
 });
 
+let searchTimer = null;
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-// Search
-
+    // Search
     const search = $('#search');
+    const searchMobile = $('#search-mobile');
     let searchHasResults = false;
-    let searchTimer = null;
     let isHovering = false;
 
     search.on('keyup', function () {
-        clearTimeout(searchTimer);
-        let searchText = $(this).val().trim();
+        searchUser($(this).val().trim());
+    });
 
-        if (searchText === "") {
-            $('.searchBox').hide();
-            return;
-        }
-
-        searchTimer = setTimeout(function () {
-            $.ajax({
-                type: 'GET',
-                url: apiBaseUrl() + `/v1/users/search/${searchText}`,
-                success: function (response) {
-                    if (response.users.length !== 0) {
-                        $('.searchBox').show();
-                        searchHasResults = true;
-                        // Reset results
-                        $('.searchBox ul').html('');
-                        response.users.forEach(function (user) {
-                            $('.searchBox ul').append('<li class="list-group-item align-items-center">' +
-                                `<a href="${baseUrl()}/user/${user.id}">` +
-                                `<img src="${user.avatar_url}" width="50px" height="50px" onerror="this.src='/img/noavatar.jpg'">` +
-                                `<b>${user.username}</b></a></li>`);
-                        });
-                    } else {
-                        searchHasResults = false;
-                    }
-                },
-                error: function () {
-                    $('.searchBox').hide();
-                    searchHasResults = false;
-                }
-            });
-        }, 300);
+    searchMobile.on('keyup', function () {
+        searchUser($(this).val().trim());
     });
 
     $('.searchBox').hover(function () {
@@ -75,7 +47,53 @@ document.addEventListener("DOMContentLoaded", function (event) {
             $('.searchBox').show();
     });
 
+    searchMobile.blur(function () {
+        if (!isHovering)
+            $('.searchBox').hide();
+    });
+
+    searchMobile.focus(function () {
+        if (searchHasResults)
+            $('.searchBox').show();
+    });
+
 });
+
+function searchUser(searchText) {
+    clearTimeout(searchTimer);
+
+    if (searchText === "") {
+        $('.searchBox').hide();
+        return;
+    }
+
+    searchTimer = setTimeout(function () {
+        $.ajax({
+            type: 'GET',
+            url: apiBaseUrl() + `/v1/users/search/${searchText}`,
+            success: function (response) {
+                if (response.users.length !== 0) {
+                    $('.searchBox').show();
+                    searchHasResults = true;
+                    // Reset results
+                    $('.searchBox ul').html('');
+                    response.users.forEach(function (user) {
+                        $('.searchBox ul').append('<li class="list-group-item align-items-center">' +
+                            `<a href="${baseUrl()}/user/${user.id}">` +
+                            `<img src="${user.avatar_url}" width="50px" height="50px" onerror="this.src='/img/noavatar.jpg'">` +
+                            `<b>${user.username}</b></a></li>`);
+                    });
+                } else {
+                    searchHasResults = false;
+                }
+            },
+            error: function () {
+                $('.searchBox').hide();
+                searchHasResults = false;
+            }
+        });
+    }, 300);
+}
 
 // Parse duration
 

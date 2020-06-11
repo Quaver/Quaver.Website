547d1d57722b423c768b31482099cd05fc14d2cc
AiAe <ayyayyedev@gmail.com>
2020-06-11 21:21:05 +0300

    https://github.com/Swan/Quaver.Website/issues/90

---
+++ src/static/js/home.js
@@ -0,0 +1,40 @@
+let page = 0;
+const tabs = $('.tab-pane');
+let pages = tabs.length - 1;
+
+$('.next-tab').on('click', function () {
+    if (page === pages) page = 0; else page += 1;
+
+    $.each(tabs, function (key, value) {
+        if ($(value).hasClass('show active')) {
+            $(value).removeClass('show active');
+            $(tabs[page]).addClass('show active');
+            return false;
+        }
+    });
+});
+
+$('.previous-tab').on('click', function () {
+    if (page !== 0) {
+        page -= 1;
+
+        $.each(tabs, function (key, value) {
+            if ($(value).hasClass('show active')) {
+                $(value).removeClass('show active');
+                $(tabs[page]).addClass('show active');
+                return false;
+            }
+        });
+    }
+});
+
+$('.count').each(function () {
+    const $this = $(this);
+    jQuery({Counter: 0}).animate({Counter: parseInt($this.text())}, {
+        duration: 1000,
+        easing: 'swing',
+        step: function () {
+            if ($this.text() === 0) $this.text(0); else $this.text(Math.ceil(this.Counter).toLocaleString());
+        }
+    });
+});

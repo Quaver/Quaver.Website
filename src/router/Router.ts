916e008e6d1215089ef9d98fb28f0130eaa38e73
AiAe <ayyayyedev@gmail.com>
2020-10-17 13:29:32 +0300

    Remove access to routes where you need to be logged in

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -24,28 +24,36 @@ export default class Router {
         const router: express.Router = express.Router();
 
         router.route("/").get(Home.GET);
+
         router.route("/leaderboard").get(LeaderBoard.GET);
         router.route("/leaderboard/hits").get(LeaderBoard.TotalHitsGET);
         router.route("/leaderboard/multiplayer").get(LeaderBoard.MultiplayerGET);
+
         router.route('/maps').get(Maps.MapsGET);
         router.route('/maps/load').post(Maps.MapsSearchPOST);
+
         router.route('/mapsets/queue').get(RankingQueue.GET);
         router.route("/mapset/:id").get(Maps.MapsetGET);
         router.route("/mapset/map/:id").get(Maps.MapGET);
         router.route("/mapset/map/:id/mods").get(Maps.ModsGET);
+        router.route("/mapset/:id").post(Authentication.RequireLogin, Maps.HandlePost);
+        router.route("/mapset/map/:id").post(Authentication.RequireLogin, Maps.HandlePost);
+        router.route("/mapset/map/:id/mods").post(Authentication.RequireLogin, Maps.HandlePostMods);
+
         router.route("/playlists").get(Playlists.PlaylistsGET);
         router.route("/playlists/load").post(Playlists.PlaylistsMoreGET);
         router.route("/playlist/create").get(Authentication.RequireLogin, Playlists.PlaylistCrate);
         router.route("/playlist/create").post(Authentication.RequireLogin, Playlists.PlaylistCratePOST);
         router.route("/playlist/:id").get(Playlists.GET);
-        router.route("/playlist/:id").post(Playlists.POST);
-        router.route("/playlist/:id/edit").get(Playlists.EditPlaylist);
-        router.route("/playlist/:id/edit").post(Playlists.POST);
+        router.route("/playlist/:id").post(Authentication.RequireLogin, Playlists.POST);
+        router.route("/playlist/:id/edit").get(Authentication.RequireLogin, Playlists.EditPlaylist);
+        router.route("/playlist/:id/edit").post(Authentication.RequireLogin, Playlists.POST);
         router.route("/playlist/map/add").post(Authentication.RequireLogin, Maps.PlaylistAddPOST);
         router.route("/playlist/map/remove").post(Authentication.RequireLogin, Maps.PlaylistRemoveMapPOST);
+
         router.route("/multiplayer/games").get(Multiplayer.MutliplayerGamesGET);
         router.route("/multiplayer/game/:id").get(Multiplayer.MutliplayerGameGET);
-        router.route("/multiplayer/game/:id/scores").post(Multiplayer.MultiplayerMatchPOST);
+
         router.route("/user/:id").get(Users.GET);
         router.route("/user/maps/load").post(Users.UserMapssetsPOST);
         router.route("/user/scores/load").post(Users.UserScoresPOST);
@@ -54,6 +62,7 @@ export default class Router {
         router.route("/friends").post(Authentication.RequireLogin, Friends.POST);
         router.route("/friend/add").post(Authentication.RequireLogin, Friends.AddFriendPOST);
         router.route("/friend/remove").post(Authentication.RequireLogin, Friends.RemoveFriendPOST);
+
         router.route("/settings/donator").get(Authentication.RequireLogin, Settings.GET);
         router.route("/settings/donator").post(Authentication.RequireLogin, Settings.POST);
 
@@ -71,10 +80,6 @@ export default class Router {
         router.route("/login/verify").get(Login.VerifyGET);
         router.route("/logout").get(Login.LogoutGET);
         router.route("/team").get(Team.GET);
-
-        router.route("/mapset/:id").post(Maps.HandlePost);
-        router.route("/mapset/map/:id").post(Maps.HandlePost);
-        router.route("/mapset/map/:id/mods").post(Maps.HandlePostMods);
 
         router.route("/download/:type/:id").get(Download.GET);
 

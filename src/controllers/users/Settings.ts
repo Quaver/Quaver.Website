72a859d5054fc8afcab24b3219bfbc3afe86d008
AiAe <ayyayyedev@gmail.com>
2021-01-03 21:16:22 +0200

    Player information settings: discord, twitter, twitch

--- src/controllers/users/Settings.ts
+++ src/controllers/users/Settings.ts
@@ -1,19 +1,17 @@
 import Responses from "../../utils/Responses";
 import Logger from "../../logging/Logger";
 import API from "../../api/API";
-
-const request = require("request");
-const config = require("../../config/config.json");
+import SqlDatabase from "../../database/SqlDatabase";
 
 export default class Settings {
+    public static validNames = ["discord", "twitter", "twitch"];
+
     public static async GET(req: any, res: any): Promise<void> {
         try {
             const user = await Settings.FetchUser(req, req.user.id);
 
-            Responses.Send(req, res, "user/settings", `Donator Settings | Quaver`, {
-                bio: user.info.userpage,
-                canChange: user.change,
-                discord: config.discord.discordUrl
+            Responses.Send(req, res, "user/settings", `Settings | Quaver`, {
+                information: user.info.information,
             });
         } catch (err) {
             Logger.Error(err);
@@ -23,69 +21,26 @@ export default class Settings {
 
     public static async POST(req: any, res: any): Promise<void> {
         try {
-            const token = await API.GetToken(req);
-            let headers: any = {};
+            const user = await Settings.FetchUser(req, req.user.id);
 
-            if (token)
-                headers["Authorization"] = `Bearer ${token}`;
+            let information = user.info.information;
 
-            if (req.body) {
-                let response: any = null;
-
-                if (req.body.submitAboutme !== undefined) {
-                    if (req.body.aboutme.length > 3000) {
-                        req.flash('error', 'Your userpage must not be greater than 3,000 characters.');
-                    } else {
-                        response = await new Promise(resolve => {
-                            request.post(`${config.apiBaseUrl}/v1/users/profile/userpage`, {
-                                form: {
-                                    userpage: req.body.aboutme.trim()
-                                },
-                                headers: headers,
-                                json: true
-                            }, function (error, response, body) {
-                                resolve(body);
-                            });
-                        }).then(body => body);
+            if (req.body.information) {
+                for (const [key, value] of Object.entries(req.body.information)) {
+                    // Validate allowed inputs
+                    if (Settings.validNames.includes(key)) {
+                        // Replace any unwanted content
+                        information[key] = Settings.htmlEntities(value);
                     }
-                } else if (req.body.submitCover !== undefined) {
-                    if (req.files && req.files.profile_cover) {
-                        req.files.profile_cover.data = Buffer.from(req.files.profile_cover.data).toString("base64");
-
-                        response = await new Promise(resolve => {
-                            request.post(`${config.apiBaseUrl}/v1/users/profile/cover`, {
-                                form: {
-                                    cover: req.files.profile_cover
-                                },
-                                headers: headers,
-                                json: true
-                            }, function (error, response, body) {
-                                resolve(body);
-                            });
-                        }).then(body => body);
-                    }
-                } else if (req.body.submitUsername !== undefined) {
-                    response = await new Promise(resolve => {
-                        request.post(`${config.apiBaseUrl}/v1/users/profile/username`, {
-                            form: {
-                                username: req.body.username
-                            },
-                            headers: headers,
-                            json: true
-                        }, function (error, response, body) {
-                            resolve(body);
-                        });
-                    }).then(body => body);
                 }
 
-                if (response) {
-                    if (response.status == 200) req.flash('success', response.message);
-                    else if (response.message) req.flash('error', response.message);
-                    else if (response.error) req.flash('error', response.error);
-                }
+                await SqlDatabase.Execute("UPDATE users SET information = ? WHERE id = ?", [JSON.stringify(information), req.user.id]);
+                req.flash('success', "Profile updated successfully.");
+            } else {
+                req.flash('error', "Something went wrong.");
             }
 
-            res.redirect(301, `/settings/donator`);
+            res.redirect(301, `/settings`);
         } catch (err) {
             Logger.Error(err);
             Responses.Return500(req, res);
@@ -98,8 +53,13 @@ export default class Settings {
         if (response.status != 200)
             return null;
 
-        response.user.change = await API.GET(req, `v1/users/profile/username`);
-
         return response.user;
+    }
+
+    static htmlEntities(str: any) {
+        return String(str).replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;');
     }
 }

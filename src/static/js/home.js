fb82c366e1b1baeab65d5daab7cdf9b53822e036
AiAe <bgdppeu@gmail.com>
2021-05-08 15:05:45 +0300

    Convert long number into abbreviated string

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -31,14 +31,34 @@ $('.previous-tab').on('click', function () {
 $('.count').each(function () {
     const $this = $(this);
     const number = parseInt($this.text());
-    jQuery({Counter: 0}).animate({Counter: parseInt($this.text())}, {
-        duration: 1000,
-        easing: 'swing',
-        step: function () {
-            if ($this.text() === 0) $this.text(0); else $this.text(Math.ceil(this.Counter).toLocaleString());
-        },
-        complete: function () {
-            $this.text(number.toLocaleString())
-        }
-    });
+
+    if (number > 999999) {
+        $this.text(abbreviateNumber(number));
+    } else {
+        jQuery({Counter: 0}).animate({Counter: parseInt($this.text())}, {
+            duration: 1000,
+            easing: 'swing',
+            step: function () {
+                if ($this.text() === 0) $this.text(0); else $this.text(Math.ceil(this.Counter).toLocaleString());
+            },
+            complete: function () {
+                $this.text(number.toLocaleString())
+            }
+        });
+    }
 });
+
+function abbreviateNumber(num, fixed = 0) {
+    if (num === null) return null;
+    if (num === 0) return '0';
+
+    fixed = (!fixed || fixed < 0) ? 0 : fixed;
+
+    let b = (num).toPrecision(2).split("e"),
+        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3),
+        c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed),
+        d = c < 0 ? c : Math.abs(c),
+        e = d + ['', 'K', 'M', 'B', 'T'][k];
+
+    return e;
+}

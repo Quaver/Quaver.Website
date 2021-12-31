a620c37a96ea37eed00bae3e5a459be77c125d8d
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-12-31 03:11:09 +0100

    Update ColorHelper.ts

--- src/utils/ColorHelper.ts
+++ src/utils/ColorHelper.ts
@@ -1,12 +1,12 @@
 export default class ColorHelper {
     public static RatingColor(rating: number): string {
-        if (rating < 2) return 'difficulty-rating-beginner';
-        else if (rating < 5) return 'difficulty-rating-easy';
-        else if (rating < 20) return 'difficulty-rating-normal';
-        else if (rating < 40) return 'difficulty-rating-hard';
-        else if (rating < 60) return 'difficulty-rating-insane';
-        else if (rating < 80) return 'difficulty-rating-expert';
-        else if (rating < 100) return 'difficulty-rating-extreme';
+        if (rating < 1) return 'difficulty-rating-beginner';
+        else if (rating < 2.5) return 'difficulty-rating-easy';
+        else if (rating < 10) return 'difficulty-rating-normal';
+        else if (rating < 20) return 'difficulty-rating-hard';
+        else if (rating < 30) return 'difficulty-rating-insane';
+        else if (rating < 40) return 'difficulty-rating-expert';
+        else if (rating < 50) return 'difficulty-rating-extreme';
 
         return 'difficulty-rating-master';
     }

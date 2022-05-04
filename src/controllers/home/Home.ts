5d167c44c150c70cc82074cdfc25306637bf2b2a
Daniel Dimitrov <bgdppeu@gmail.com>
2022-05-04 16:07:39 +0300

    Update Home.ts

--- src/controllers/home/Home.ts
+++ src/controllers/home/Home.ts
@@ -12,13 +12,10 @@ export default class Home {
      */
     public static async GET(req: any, res: any): Promise<void> {
         try {
-            //const recentlyRanked = await Home.FetchRecentlyRanked(req);
-            const recentlyRanked = [];
-            //const stats = await API.GET(req, "v1/stats");
-            const stats = [];
+            const recentlyRanked = await Home.FetchRecentlyRanked(req);
+            const stats = await API.GET(req, "v1/stats");
             const blog = await axios.get("https://blog.quavergame.com/feed.json");
-            //const popularMapsets = await API.GET(req, "v1/stats/mapsets/mostplayed");
-            const popularMapsets = [];
+            const popularMapsets = await API.GET(req, "v1/stats/mapsets/mostplayed");
             
             return Responses.Send(req, res, "home", "Home | Quaver", {
                 recentlyRanked: recentlyRanked,

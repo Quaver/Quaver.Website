8a97612bf2b3f68cd39a6010115d0b37a546440f
Adri <adrien.boitelle@gmail.com>
2026-01-07 13:32:13 +0100

    Fix key count display

--- src/utils/GameModeHelper.ts
+++ src/utils/GameModeHelper.ts
@@ -1,13 +1,32 @@
 export default class GameModeHelper {
     /**
+     * Maps GameMode ID to key count if it differs
+     */
+    private static MODE_KEY = {
+        1: 4,
+        2: 7,
+        3: 1,
+        4: 2,
+        5: 3,
+        6: 5,
+        7: 6,
+    };
+
+    /**
+     * Convert game mode to key count
+     * @constructor
+     * @param gm
+     */
+    public static gameModeKey(gm: number): number {
+        return gm in GameModeHelper.MODE_KEY ? GameModeHelper.MODE_KEY[gm] : gm;
+    }
+
+    /**
      * Convert game mode to string
      * @constructor
      * @param gm
      */
     public static gameMode(gm: number): string {
-        if (gm == 1)
-            return '4K';
-        else
-            return '7K';
+        return `${GameModeHelper.gameModeKey(gm)}K`;
     }
 }
